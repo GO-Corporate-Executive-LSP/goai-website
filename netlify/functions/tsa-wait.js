676f2a7451fa7c5d8d0719d70c77b97686f96de2
Dwayne Heard <gocltqc@gmail.com>
2026-02-23 00:29:51 -0500

    Update tsa-wait.js

--- netlify/functions/tsa-wait.js
+++ netlify/functions/tsa-wait.js
@@ -1,1 +1,29 @@
+exports.handler = async (event) => {
+  const airport = event.queryStringParameters.airport;
 
+  if (!airport) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: "Airport code required" })
+    };
+  }
+
+  try {
+    const res = await fetch(
+      `https://apps.tsa.dhs.gov/MyTSAWebService/GetConfirmedWaitTimes.ashx?ap=${airport}&output=json`
+    );
+
+    const data = await res.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "TSA fetch failed" })
+    };
+  }
+};
