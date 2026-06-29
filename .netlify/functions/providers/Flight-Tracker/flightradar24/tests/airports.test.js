6eecfee9d0d165c52d1611acebe7915134a97f7e
GÖ.AI <gocltqc@gmail.com>
2026-06-29 02:22:38 -0400

    Add files via upload

---
+++ .netlify/functions/providers/Flight-Tracker/flightradar24/tests/airports.test.js
@@ -0,0 +1,7 @@
+/**
+ * GÖ.AI Backend
+ * FlightRadar24 Provider Adapter
+ * File: tests/airports.test.js
+ */
+
+"use strict";
---
+++ .netlify/functions/providers/Flight-Tracker/flightradar24/tests/arrivals.test.js
@@ -0,0 +1,7 @@
+/**
+ * GÖ.AI Backend
+ * FlightRadar24 Provider Adapter
+ * File: tests/arrivals.test.js
+ */
+
+"use strict";
