a1260d95a882f09095a33391c8960fc13b28c541
GÖ.AI <dwayne@goaihq.com>
2026-07-01 08:32:34 -0400

    Create travelWarnings.js for travel advisories
    Add RSS feed for travel advisories from travel.state.gov.

---
+++ .netlify/functions/providers/Gov't & Public Intel/state-department/resources/travelWarnings.js
@@ -0,0 +1,9 @@
+<rss xmlns:dc="http://purl.org/dc/elements/1.1/" version="2.0">
+<channel>
+<title>travel.state.gov: Travel Advisories</title>
+<link> travel.state.gov: Travel Advisories </link>
+<description> http://travel.state.gov/content/travel/en/traveladvisories/traveladvisories.html </description>
+</channel>
+</rss>
+
+https://travel.state.gov/_res/rss/TWs.xml
