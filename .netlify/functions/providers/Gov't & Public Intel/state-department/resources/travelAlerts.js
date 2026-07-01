dfcf822f4f9247e6264391ae7d68d5b11d437e30
GÖ.AI <dwayne@goaihq.com>
2026-07-01 08:30:24 -0400

    Create travelAlerts.js for travel advisories
    Add XML structure for travel advisories from travel.state.gov

---
+++ .netlify/functions/providers/Gov't & Public Intel/state-department/resources/travelAlerts.js
@@ -0,0 +1,11 @@
+This XML file does not appear to have any style information associated with it. The document tree is shown below.
+<rss xmlns:dc="http://purl.org/dc/elements/1.1/" version="2.0">
+<channel>
+<title>travel.state.gov: Travel Advisories</title>
+<link> travel.state.gov: Travel Advisories </link>
+<description> http://travel.state.gov/content/travel/en/traveladvisories/traveladvisories.html </description>
+</channel>
+</rss>
+
+
+https://travel.state.gov/_res/rss/TAs.xml
